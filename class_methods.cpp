7d73ce0e1f0bc40b168dec9b9d03229ceac9d51f
Simran Kaur <simkaur2637@gmail.com>
2026-09-06 10:59:32 +0530

    Create class_methods.cpp
    Methods are functions that belongs to the class.
    
    There are two ways to define functions that belongs to a class:
    
    Inside class definition
    Outside class definition
    along with that we can add parameters also like in functions

---
+++ class_methods.cpp
@@ -0,0 +1,53 @@
+//Define a Method Inside the Class
+class MyClass {        // The class
+  public:              // Access specifier
+    void myMethod() {  // Method/function defined inside the class
+      cout << "Hello World!";
+    }
+};
+
+int main() {
+  MyClass myObj;     // Create an object of MyClass
+  myObj.myMethod();  // Call the method
+  return 0;
+}
+
+
+
+//Define a Method Outside the Class
+class MyClass {        // The class
+  public:              // Access specifier
+    void myMethod();   // Method/function declaration
+};
+
+// Method/function definition outside the class
+void MyClass::myMethod() {
+  cout << "Hello World!";
+}
+
+int main() {
+  MyClass myObj;     // Create an object of MyClass
+  myObj.myMethod();  // Call the method
+  return 0;
+
+  
+
+  //You can also pass values to methods just like regular functions
+
+
+class Car {
+  public:
+    int speed(int maxSpeed);
+};
+
+int Car::speed(int maxSpeed) {
+  return maxSpeed;
+}
+
+int main() {
+  Car myObj; // Create an object of Car
+  cout << myObj.speed(200); // Call the method with an argument
+  return 0;
+}
+  
+}
